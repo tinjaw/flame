deaa55c6956eadc6dd45b8509e480df38fbb3414
Chaim Krause <chaim@chaim.com>
2021-08-08 12:56:08 -0500

    Bring in upstream. (#1)
    * Added support for Steam URLs. Changed default prefix setting options to be dynamically rendered
    
    * Fixed bug with custom css not persisting
    
    * Fixed custom icons not updating
    
    * Custom icons for bookmarks
    
    * Added changelog. Added curl to Docker image
    
    * Use correct changelog link
    
    current link in release 1.6.1 points to an incorrect url that returns 404
    
    * github directory name changed
    
    * docker api
    
    * fix optional docker.sock mount
    
    * Pushed version 1.6.2. Small formatting fixes
    
    Co-authored-by: unknown <pawel999@icloud.com>
    Co-authored-by: Bhanu <itsbhanusharma@users.noreply.github.com>
    Co-authored-by: Pablo Ruiz <pruizlezcano@gmail.com>

--- controllers/apps.js
+++ controllers/apps.js
@@ -3,6 +3,9 @@ const ErrorResponse = require('../utils/ErrorResponse');
 const App = require('../models/App');
 const Config = require('../models/Config');
 const { Sequelize } = require('sequelize');
+const axios = require('axios');
+const Logger = require('../utils/Logger');
+const logger = new Logger();
 
 // @desc      Create new app
 // @route     POST /api/apps
@@ -20,13 +23,12 @@ exports.createApp = asyncWrapper(async (req, res, next) => {
     _body.icon = req.file.filename;
   }
 
-
   if (pinApps) {
     if (parseInt(pinApps.value)) {
       app = await App.create({
         ..._body,
         isPinned: true
-      })
+      });
     } else {
       app = await App.create(req.body);
     }
@@ -35,8 +37,8 @@ exports.createApp = asyncWrapper(async (req, res, next) => {
   res.status(201).json({
     success: true,
     data: app
-  })
-})
+  });
+});
 
 // @desc      Get all apps
 // @route     GET /api/apps
@@ -46,25 +48,90 @@ exports.getApps = asyncWrapper(async (req, res, next) => {
   const useOrdering = await Config.findOne({
     where: { key: 'useOrdering' }
   });
+  const useDockerApi = await Config.findOne({
+    where: { key: 'dockerApps' }
+  });
+  const unpinStoppedApps = await Config.findOne({
+    where: { key: 'unpinStoppedApps' }
+  });
 
   const orderType = useOrdering ? useOrdering.value : 'createdAt';
   let apps;
 
+  if (useDockerApi && useDockerApi.value == 1) {
+    let containers = null;
+
+    try {
+      let { data } = await axios.get(
+        'http://localhost/containers/json?{"status":["running"]}',
+        {
+          socketPath: '/var/run/docker.sock'
+        }
+      );
+      containers = data;
+    } catch {
+      logger.log("Can't connect to the docker socket", 'ERROR');
+    }
+
+    if (containers) {
+      apps = await App.findAll({
+        order: [[orderType, 'ASC']]
+      });
+
+      containers = containers.filter(e => Object.keys(e.Labels).length !== 0);
+      const dockerApps = [];
+      for (const container of containers) {
+        const labels = container.Labels;
+
+        if (
+          'flame.name' in labels &&
+          'flame.url' in labels &&
+          /^app/.test(labels['flame.type'])
+        ) {
+          dockerApps.push({
+            name: labels['flame.name'],
+            url: labels['flame.url'],
+            icon: labels['flame.icon'] || 'docker'
+          });
+        }
+      }
+
+      if (unpinStoppedApps && unpinStoppedApps.value == 1) {
+        for (const app of apps) {
+          await app.update({ isPinned: false });
+        }
+      }
+
+      for (const item of dockerApps) {
+        if (apps.some(app => app.name === item.name)) {
+          const app = apps.filter(e => e.name === item.name)[0];
+          await app.update({ ...item, isPinned: true });
+        } else {
+          await App.create({
+            ...item,
+            isPinned: true
+          });
+        }
+      }
+    }
+  }
+
   if (orderType == 'name') {
     apps = await App.findAll({
-      order: [[ Sequelize.fn('lower', Sequelize.col('name')), 'ASC' ]]
+      order: [[Sequelize.fn('lower', Sequelize.col('name')), 'ASC']]
     });
   } else {
     apps = await App.findAll({
-      order: [[ orderType, 'ASC' ]]
+      order: [[orderType, 'ASC']]
     });
   }
 
-  res.status(200).json({
+  // Set header to fetch containers info every time
+  res.status(200).setHeader('Cache-Control', 'no-store').json({
     success: true,
     data: apps
-  })
-})
+  });
+});
 
 // @desc      Get single app
 // @route     GET /api/apps/:id
@@ -75,14 +142,16 @@ exports.getApp = asyncWrapper(async (req, res, next) => {
   });
 
   if (!app) {
-    return next(new ErrorResponse(`App with id of ${req.params.id} was not found`, 404));
+    return next(
+      new ErrorResponse(`App with id of ${req.params.id} was not found`, 404)
+    );
   }
 
   res.status(200).json({
     success: true,
     data: app
-  })
-})
+  });
+});
 
 // @desc      Update app
 // @route     PUT /api/apps/:id
@@ -93,16 +162,24 @@ exports.updateApp = asyncWrapper(async (req, res, next) => {
   });
 
   if (!app) {
-    return next(new ErrorResponse(`App with id of ${req.params.id} was not found`, 404));
+    return next(
+      new ErrorResponse(`App with id of ${req.params.id} was not found`, 404)
+    );
   }
 
-  app = await app.update({ ...req.body });
+  let _body = { ...req.body };
+
+  if (req.file) {
+    _body.icon = req.file.filename;
+  }
+
+  app = await app.update(_body);
 
   res.status(200).json({
     success: true,
     data: app
-  })
-})
+  });
+});
 
 // @desc      Delete app
 // @route     DELETE /api/apps/:id
@@ -110,26 +187,29 @@ exports.updateApp = asyncWrapper(async (req, res, next) => {
 exports.deleteApp = asyncWrapper(async (req, res, next) => {
   await App.destroy({
     where: { id: req.params.id }
-  })
+  });
 
   res.status(200).json({
     success: true,
     data: {}
-  })
-})
+  });
+});
 
 // @desc      Reorder apps
 // @route     PUT /api/apps/0/reorder
 // @access    Public
 exports.reorderApps = asyncWrapper(async (req, res, next) => {
   req.body.apps.forEach(async ({ id, orderId }) => {
-    await App.update({ orderId }, {
-      where: { id }
-    })
-  })
+    await App.update(
+      { orderId },
+      {
+        where: { id }
+      }
+    );
+  });
 
   res.status(200).json({
     success: true,
     data: {}
-  })
-})
+  });
+});
